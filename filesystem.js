b1fa00c70cee434562656c854d6b1af94e25a7f1
Maaz Ali <moaaxali@gmail.com>
2021-03-26 22:32:33 +0500

    Correct the directory path to tmp/temp.js

--- filesystem.js
+++ filesystem.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 
 try {
-  fs.unlinkSync('/tmp/temp.js');
+  fs.unlinkSync('tmp/temp.js');
   console.log('successfully deleted /tmp/temp.js');
 } catch (err) {
   console.log(err);
